405d915de4ed9284daaf43f3d18877b829cd9ead
claramingyue <124404447+claramingyue@users.noreply.github.com>
2024-10-18 13:00:45 +0200

    prepare for jpg training

--- DDPM.py
+++ DDPM.py
@@ -26,47 +26,47 @@ def guardar_modelo(diffusion, model_path):
     torch.save(diffusion.state_dict(), model_path)
     print(f"Modelo guardado en {model_path}")
 
-# Cargar el modelo guardado
-def cargar_modelo(model_path, model):
-    loaded_diffusion = GaussianDiffusion(
-        model,
-        image_size=256,
-        timesteps=1000,
-        sampling_timesteps=250
-    )
-    loaded_diffusion.load_state_dict(torch.load(model_path))
-    print(f"Modelo cargado desde {model_path}")
-    return loaded_diffusion
+# # Cargar el modelo guardado
+# def cargar_modelo(model_path, model):
+#     loaded_diffusion = GaussianDiffusion(
+#         model,
+#         image_size=256,
+#         timesteps=1000,
+#         sampling_timesteps=250
+#     )
+#     loaded_diffusion.load_state_dict(torch.load(model_path))
+#     print(f"Modelo cargado desde {model_path}")
+#     return loaded_diffusion
 
-# Generar imágenes
-def generar_imagenes(diffusion, output_dir, num_images_to_generate=100):
-    os.makedirs(output_dir, exist_ok=True)
-    images_generated = 0
+# # Generar imágenes
+# def generar_imagenes(diffusion, output_dir, num_images_to_generate=100):
+#     os.makedirs(output_dir, exist_ok=True)
+#     images_generated = 0
 
-    # Mover el modelo a la GPU si está disponible
-    if torch.cuda.is_available():
-        diffusion.to('cuda')
+#     # Mover el modelo a la GPU si está disponible
+#     if torch.cuda.is_available():
+#         diffusion.to('cuda')
 
-    while images_generated < num_images_to_generate:
-        batch_images = diffusion.sample(batch_size=10)  # Cambia el tamaño del lote si tu GPU lo permite
-        for i in range(batch_images.shape[0]):
-            img = batch_images[i].permute(1, 2, 0).cpu().numpy()  # Cambiar formato de tensor a imagen
-            fpath = f'AAimage_{images_generated}-{images_generated + i}.png'
-            img_path = os.path.join(output_dir, fpath)
-            plt.imsave(img_path, img)
-            print(f"Generated: {img_path}")
-        images_generated += batch_images.shape[0]
+#     while images_generated < num_images_to_generate:
+#         batch_images = diffusion.sample(batch_size=10)  # Cambia el tamaño del lote si tu GPU lo permite
+#         for i in range(batch_images.shape[0]):
+#             img = batch_images[i].permute(1, 2, 0).cpu().numpy()  # Cambiar formato de tensor a imagen
+#             fpath = f'AAimage_{images_generated}-{images_generated + i}.png'
+#             img_path = os.path.join(output_dir, fpath)
+#             plt.imsave(img_path, img)
+#             print(f"Generated: {img_path}")
+#         images_generated += batch_images.shape[0]
 
-    print(f"Imágenes generadas y guardadas en {output_dir}")
+#     print(f"Imágenes generadas y guardadas en {output_dir}")
 
 def main():
     # Definir rutas
     # dataset_path = Path('/content/drive/MyDrive/dataset_preprocesado_png')
     # model_path = '/content/drive/MyDrive/trained_diffusion_model_png.pth'
     # output_dir = '/content/drive/MyDrive/Colab_Notebooks/SynteticImageAnalysis/DDPM_results_png'
-    dataset_path = Path('/Neuronal/dataset_preprocesado_png')
-    model_path = '/home/claramingyue/DDPM/trained_diffusion_model_png.pth'
-    output_dir = '/home/claramingyue/DDPM/SynteticImageAnalysis/DDPM_results_png'
+    dataset_path = Path('/Neuronal/bedroom_dataset_preprocesado256')
+    model_path = '/home/claramingyue/DDPM/modelo_jpg'
+    #output_dir = '/home/claramingyue/DDPM/SynteticImageAnalysis/DDPM_results_png'
 
     # Verificar si la carpeta del dataset existe
     if not dataset_path.exists():
@@ -82,7 +82,7 @@ def main():
         str(dataset_path),
         train_batch_size=16,
         train_lr=8e-5,
-        train_num_steps=8000,
+        train_num_steps=10000,
         gradient_accumulate_every=2,
         ema_decay=0.995,
         amp=True,
